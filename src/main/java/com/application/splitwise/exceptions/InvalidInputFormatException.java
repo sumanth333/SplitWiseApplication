78f70a61c6ea03d21d708741b258ccbcc538974a
sumanth333 <polesumanth333@gmail.com>
2022-05-06 13:29:21 +0530

    [Sumanth] Create possible Custom Exceptions

---
+++ src/main/java/com/application/splitwise/exceptions/InvalidInputFormatException.java
@@ -0,0 +1,7 @@
+package com.application.splitwise.exceptions;
+
+public class InvalidInputFormatException extends Exception{
+    public InvalidInputFormatException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/application/splitwise/exceptions/InvalidInputValueException.java
@@ -0,0 +1,7 @@
+package com.application.splitwise.exceptions;
+
+public class InvalidInputValueException extends Exception{
+    public InvalidInputValueException(String message) {
+        super(message);
+    }
+}
