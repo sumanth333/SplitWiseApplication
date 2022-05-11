abf793b8a20b9e67a2cfb7a610e492d919b0f6bb
sumanth333 <polesumanth333@gmail.com>
2022-05-11 12:55:56 +0530

    [Sumanth] Remove unused custom exception

--- src/main/java/com/application/splitwise/exceptions/EmptyExpenditureDataException.java
+++
@@ -1,7 +0,0 @@
-package com.application.splitwise.exceptions;
-
-public class EmptyExpenditureDataException extends Exception{
-    public EmptyExpenditureDataException(String message) {
-        super(message);
-    }
-}
